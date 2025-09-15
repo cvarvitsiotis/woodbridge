763c9cb21d6dde9ace0167bd02215a0d326b1805
Chris Varvitsiotis <cvarvitsiotis@gmail.com>
2025-09-15 09:56:14 -0700

    Print map

--- src/app/racingInfo/courseAndVenueMap/page.tsx
+++ src/app/racingInfo/courseAndVenueMap/page.tsx
@@ -4,6 +4,7 @@ import woodbridgeCourse from "@/../public/images/woodbridge-course.png";
 import { Metadata } from "next";
 import { pages } from "@/config/site";
 import PageHeader from "@/components/pageHeader";
+import { Link } from "@heroui/link";
 
 export const metadata: Metadata = {
   title: pages.courseAndVenueMap.menuLabel,
@@ -15,7 +16,12 @@ export default async function Page() {
       <PageHeader>
         <span className="font-bold">Course</span> & <span className="font-bold">Venue</span> Map
       </PageHeader>
-      <div className="pt-10">
+      <div className="space-y-3 pt-7">
+        <div className="mx-auto max-w-[800px] text-right">
+          <Link isExternal href="/images/woodbridge-course.png" className="font-semibold">
+            [ PRINT ]
+          </Link>
+        </div>
         <div className="relative mx-auto aspect-[4196/3246] h-full max-h-[90dvh] max-w-[800px] lg:max-h-[70dvh]">
           <Image
             fill
