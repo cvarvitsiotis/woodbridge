2502ab50af7c253639577b1855430548f84d0d70
Chris Varvitsiotis <cvarvitsiotis@gmail.com>
2026-09-06 17:38:03 -0700

    Schedule race num

--- src/components/styledTableCell.tsx
+++ src/components/styledTableCell.tsx
@@ -1,6 +1,13 @@
 import { Table } from "@heroui/react";
 import { ReactNode } from "react";
+import clsx from "clsx";
 
-export default function StyledTableCell({ children }: { children: ReactNode }) {
-  return <Table.Cell className="py-2">{children}</Table.Cell>;
+export default function StyledTableCell({
+  className,
+  children,
+}: {
+  className?: string;
+  children: ReactNode;
+}) {
+  return <Table.Cell className={clsx("py-2", className)}>{children}</Table.Cell>;
 }
